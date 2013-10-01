0a487133473cd1fe43df2c0c26d681087aaa5a1e
salot romain <rsalot@prune>
2013-10-01 13:54:33 +0200

    5eme Histoire (non finie)

--- src/TestYinch.java
+++ src/TestYinch.java
@@ -8,13 +8,15 @@ public class TestYinch extends TestCase {
 
 	public void test_couleur() { // test la couleur de départ de l'anneau
 		Yinch y = new Yinch();
-		assertTrue(y.current_color() == Yinch.color.WHITE || y.current_color() == Yinch.color.BLACK);
+		assertTrue(y.current_color() == Yinch.color.WHITE
+				|| y.current_color() == Yinch.color.BLACK);
 	}
 
-	public void test_ring() throws GrilleException, RingCouleurException, RingIntersecException { // test si
-																	// un anneau
-																	// est
-		// placé dans la grille
+	public void test_ring() throws GrilleException, RingCouleurException,
+			RingIntersecException { /*
+									 * test si un anneau est placé dans la
+									 * grille
+									 */
 		Yinch y = new Yinch();
 		assertTrue(y.put_ring('A', 2, Yinch.color.WHITE) == 2);
 		assertTrue(y.put_ring('A', 3, Yinch.color.BLACK) == 1);
@@ -26,12 +28,11 @@ public class TestYinch extends TestCase {
 
 	}
 
-	public void test_un_ring() throws GrilleException, RingCouleurException, RingIntersecException { // test
-																		// si
-																		// après
-		// insertion d'un
-		// anneau, il y'a un
-		// anneau sur le grille
+	public void test_un_ring() throws GrilleException, RingCouleurException,
+			RingIntersecException { /*
+									 * test si après insertion d'un anneau, il
+									 * y'a un anneau sur le grille
+									 */
 		Yinch y = new Yinch();
 		assertTrue(y.put_ring('A', 3, Yinch.color.BLACK) == 1);
 		assertTrue(y.etat_init() == 1);
@@ -48,7 +49,8 @@ public class TestYinch extends TestCase {
 		}
 	}
 
-	public void test_meme_couleur() throws GrilleException, RingCouleurException, RingIntersecException {
+	public void test_meme_couleur() throws GrilleException,
+			RingCouleurException, RingIntersecException {
 		Yinch y = new Yinch();
 		try {
 			y.put_ring('A', 3, Yinch.color.BLACK);
@@ -58,8 +60,9 @@ public class TestYinch extends TestCase {
 			assertTrue(true);
 		}
 	}
-	
-	public void test_intersection() throws GrilleException, RingCouleurException, RingIntersecException{
+
+	public void test_intersection() throws GrilleException,
+			RingCouleurException, RingIntersecException {
 		Yinch y = new Yinch();
 		try {
 			y.put_ring('A', 3, Yinch.color.BLACK);
@@ -68,6 +71,54 @@ public class TestYinch extends TestCase {
 		} catch (RingIntersecException e) {
 			assertTrue(true);
 		}
+	}
+
+	public void test_is_initialized() throws GrilleException,
+			RingCouleurException, RingIntersecException {
+		Yinch y = new Yinch();
+		try {
+			y.put_ring('A', 3, Yinch.color.BLACK);
+			y.put_ring('A', 4, Yinch.color.WHITE);
+			y.put_ring('B', 3, Yinch.color.BLACK);
+			y.put_ring('B', 4, Yinch.color.WHITE);
+			y.put_ring('C', 3, Yinch.color.BLACK);
+			y.put_ring('C', 4, Yinch.color.WHITE);
+			y.put_ring('D', 3, Yinch.color.BLACK);
+			y.put_ring('D', 4, Yinch.color.WHITE);
+			y.put_ring('E', 3, Yinch.color.BLACK);
+			y.put_ring('E', 4, Yinch.color.WHITE);
+			assertTrue(y.is_initialized() == true);
+		} catch (RingIntersecException e) {
+			assertTrue(true);
+		}
+	}
+
+	public void test_put_marker() throws MarkerException {
+		Yinch y = new Yinch();
+		try {
+		assertTrue(y.put_marker('D', 2, Yinch.color.BLACK) == 1);
+		}
+		catch(MarkerException e) {
+			assertTrue(true);
+		}
+
+	}
+	
+	public void test_move_ring() throws GrilleException, RingCouleurException, RingIntersecException, MoveRingException, MarkerException {
+		Yinch y = new Yinch();
+		
+		y.put_ring('D', 6, Yinch.color.WHITE);
+
+		
+		try{
+			y.put_ring('D', 2, Yinch.color.BLACK);
+			y.put_marker('D', 2, Yinch.color.BLACK);
+			assertTrue(y.move_ring('D', 2, 'D', 6) == 1);
+		}
+		catch(MoveRingException e){
+			assertTrue(true);
+		}
+
 	}
 
 }
--- src/Yinch.java
+++ src/Yinch.java
@@ -5,18 +5,19 @@ import src.exceptions.*;
 public class Yinch {
 
 	private int[][] jeu;
+	private int[][] marker;
 
 	public Yinch() {
 		jeu = new int[11][11];
+		marker = new int[11][11];
 	}
 
 	public enum color {
 		BLACK, WHITE
 	};
-	
+
 	color old = null;
 
-	
 	public color current_color() {
 		int setColor = pRand(2);
 		if (setColor == 1) {
@@ -29,19 +30,22 @@ public class Yinch {
 
 	}
 
-	public int put_ring(char colonne, int ligne, color couleur) throws GrilleException, RingCouleurException, RingIntersecException{
-		int c =0;
-		if(couleur == old)throw new RingCouleurException();
-		if(couleur==color.BLACK){
-			c=1;
+	public int put_ring(char colonne, int ligne, color couleur)
+			throws GrilleException, RingCouleurException, RingIntersecException {
+		int c = 0;
+		if (couleur == old)
+			throw new RingCouleurException();
+		if (couleur == color.BLACK) {
+			c = 1;
 			old = color.BLACK;
-		}
-		else{
-			c=2;
+		} else {
+			c = 2;
 			old = color.WHITE;
 		}
-		if(colonne=='A' && (ligne <2 || ligne>5))throw new GrilleException();
-		if(jeu[(int) (colonne - 'A')][ligne - 1] != 0)throw new RingIntersecException();
+		if (colonne == 'A' && (ligne < 2 || ligne > 5))
+			throw new GrilleException();
+		if (jeu[(int) (colonne - 'A')][ligne - 1] != 0)
+			throw new RingIntersecException();
 		return jeu[(int) (colonne - 'A')][ligne - 1] = c;
 
 	}
@@ -51,15 +55,52 @@ public class Yinch {
 		for (int i = 0; i < jeu.length; i++) {
 			for (int j = 0; j < jeu[i].length; j++) {
 				if (jeu[i][j] != 0) {
-					cpt ++;
+					cpt++;
 				}
 			}
 		}
 		return cpt;
 	}
-	
 
-	
+	public boolean is_initialized() {
+		int cpt = etat_init();
+		if (cpt == 10) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public int put_marker(char colonne, int ligne, color couleur)
+			throws MarkerException {
+		int c = 0;
+		if (couleur == color.BLACK) {
+			c = 1;
+			old = color.BLACK;
+		} else {
+			c = 2;
+			old = color.WHITE;
+		}
+		if (jeu[(int) (colonne - 'A')][ligne - 1] != c) {
+			throw new MarkerException();
+		} else {
+			return marker[(int) (colonne - 'A')][ligne - 1] = c;
+		}
+	}
+
+	public int move_ring(char col_now, int lig_now, char col_next, int lig_next)
+			throws MoveRingException {
+		int c = 0; // récupération de la couleur.
+		c = jeu[(int) (col_now - 'A')][lig_now - 1];
+		if (jeu[(int) (col_next - 'A')][lig_next - 1] != 0) {
+			throw new MoveRingException();
+		} else {
+			jeu[(int) (col_now - 'A')][lig_now - 1] = 0;
+			return jeu[(int) (col_next - 'A')][lig_next - 1] = c;
+		}
+	}
+
 	static Random rand = new Random();
 
 	static int pRand(int mod) {
